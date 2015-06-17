24a91822082bb2d24a3dfc724319a7870ad2ee27
Thomas Proenza <tomproenza@Thomass-MacBook-Pro.local>
2015-06-16 23:38:56 -0400

    recommendation engine logic

--- browser/js/orders/orders.js
+++ browser/js/orders/orders.js
@@ -12,7 +12,9 @@ app.config(function ($stateProvider) {
 app.controller('OrdersController', function ($scope, OrdersFactory, BlendsFactory){
 
 	$scope.allOrders = null;
-
+	$scope.microName = null;
+	$scope.randomMicro = null;
+	$scope.recommendedBlend = null;
 
 	OrdersFactory.getAllOrders().then(function (orders) {
 		$scope.allOrders = orders;
@@ -26,9 +28,42 @@ app.controller('OrdersController', function ($scope, OrdersFactory, BlendsFactor
 
 	$scope.showOrdersById = function (orderid) {
 		OrdersFactory.getOrderById(orderid).then(function (order) {
+			console.log('the order:', order);
 			$scope.order = order;
+			BlendsFactory.getBlendById(order.blend[0].typeofblend)
+			.then(function (blend) {
+				
+				console.log('the micro array', blend.micros);
+				
+				$scope.microName = blend.micros.map(function(obj){
+					return obj.name;
+				})
+
+				$scope.randomMicro = $scope.microName[Math.floor(Math.random()*$scope.microName.length)];
+				console.log('this is blend ordered', blend)
+
+				BlendsFactory.getAllBlends().then(function (blends) {
+					console.log('all the blends', blends);
+					
+					$scope.matchedBlends = blends.filter(function(blend){
+						var hasRandomMicro = false;
+						blend.micros.forEach(function(micro){
+							if(micro.name === $scope.randomMicro){
+								hasRandomMicro = true;
+							}
+						})
+						return hasRandomMicro
+					}
+					);
+					
+					$scope.recommendedBlend = $scope.matchedBlends[Math.floor(Math.random()*$scope.matchedBlends.length)]
+
+					
+
+				})
+			})
 		});
-	};
+	}
 
 	$scope.addOrder = function (order) {
 
